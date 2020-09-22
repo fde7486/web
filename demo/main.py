0ed93a34b475a1b656a4d357d35343f55afaaaad
fde7486 <family711ok@gamil.com>
2020-09-22 15:13:27 +0800

    for迴圈

--- demo/main.py
+++ demo/main.py
@@ -9,3 +9,8 @@ def index():
 @app.route('/hello/<name>')
 def hello(name = None):
     return render_template('hello.html', name = name)
+
+@app.route('/loop')
+@app.route('/loop/<int:n>')
+def loop(n=6):
+    return render_template('loop.html',n=n)
